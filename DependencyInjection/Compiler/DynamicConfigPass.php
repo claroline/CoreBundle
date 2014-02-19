claroline/CoreBundle

commit 6c35dcc3e62344a1219d897d4b7de9675989a82b

style

DependencyInjection/Compiler/DynamicConfigPass.php
@@ -45,11 +45,8 @@ class DynamicConfigPass implements CompilerPassInterface
         $container->setDefinition('session.handler', $handler);
 
         //cookie lifetime
-
-        if ($container->getParameter('kernel.environment') !== 'test') {
-            $storage = $container->findDefinition('session.storage');
-            $storage->addMethodCall('setOptions', array(new Reference('claroline.session.storage_options')));
-        }
+        $storage = $container->findDefinition('session.storage');
+        $storage->addMethodCall('setOptions', array(new Reference('claroline.session.storage_options')));
 
         //notification
         $container->setAlias('icap.notification.orm.entity_manager', 'claroline.persistence.object_manager');
@@ -57,7 +54,7 @@ class DynamicConfigPass implements CompilerPassInterface
         //facebook
         $facebook = new Definition();
         $facebook->setFactoryService('claroline.hwi.resource_owner_factory');
-        $facebook->setFactoryMethod('getFacebook');
+        $facebook->setFactoryMethod('getFacebookResourceOwner');
         $facebook->setClass('FacebookResourceOwner');
         $container->removeDefinition('hwi_oauth.resource_owner.facebook');
         $container->setDefinition('hwi_oauth.resource_owner.facebook', $facebook);

Controller/Administration/ParametersController.php
@@ -25,6 +25,7 @@ use JMS\DiExtraBundle\Annotation as DI;
 use JMS\SecurityExtraBundle\Annotation\PreAuthorize;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormFactory;
 use Symfony\Component\HttpFoundation\Request;
@@ -154,6 +155,7 @@ class ParametersController extends Controller
      * )
      *
      * @Template("ClarolineCoreBundle:Administration\platform:settings.html.twig")
+     * @Method("POST")
      *
      * Updates the platform settings and redirects to the settings form.
      *
@@ -246,6 +248,8 @@ class ParametersController extends Controller
      *     name="claro_admin_edit_parameters_appearance"
      * )
      *
+     * @Method("POST")
+     *
      * Displays the platform settings.
      *
      * @return \Symfony\Component\HttpFoundation\Response
@@ -321,6 +325,7 @@ class ParametersController extends Controller
      * )
      *
      * @Template("ClarolineCoreBundle:Administration\platform\mail:server.html.twig")
+     * @Method("POST")
      *
      * Updates the platform settings and redirects to the settings form.
      *
@@ -412,6 +417,7 @@ class ParametersController extends Controller
      * )
      *
      * @Template("ClarolineCoreBundle:Administration\platform\mail:registration.html.twig")
+     * @Method("POST")
      *
      * @return \Symfony\Component\HttpFoundation\Response
      *
@@ -472,6 +478,7 @@ class ParametersController extends Controller
      * )
      *
      * @Template("ClarolineCoreBundle:Administration\platform\mail:layout.html.twig")
+     * @Method("POST")
      *
      * @return \Symfony\Component\HttpFoundation\Response
      *
@@ -529,6 +536,7 @@ class ParametersController extends Controller
      * Updates the platform settings and redirects to the settings form.
      *
      * @Route("/terms_of_service/submit", name="claro_admin_edit_terms_of_service_submit")
+     * @Method("POST")
      *
      * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
@@ -623,6 +631,7 @@ class ParametersController extends Controller
      * )
      *
      * @Template("ClarolineCoreBundle:Administration\platform:session.html.twig")
+     * @Method("POST")
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
@@ -708,6 +717,7 @@ class ParametersController extends Controller
      *     "/oauth/facebook/submit",
      *     name="claro_admin_facebook_form_submit"
      * )
+     * @Method("POST")
      *
      * Displays the administration section index.
      *

Controller/hwi/SecurityControlle.php
@@ -1,14 +1,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: jorge
- * Date: 6/02/14
- * Time: 15:07
- */
-
-namespace Claroline\CoreBundle\Controller\hwi;
-
-
-class SecurityController {
-
-} 

Form/Administration/FacebookType.php
@@ -27,7 +27,7 @@ class FacebookType extends AbstractType
 
     public function getName()
     {
-        return 'platform_parameters_form';
+        return 'platform_facebook_application_form';
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)

Library/Hwi/ResourceOwnerFactory.php
@@ -13,9 +13,9 @@ namespace Claroline\CoreBundle\Library\Hwi;
 
 use Claroline\CoreBundle\Library\Configuration\PlatformConfigurationHandler;
 use JMS\DiExtraBundle\Annotation as DI;
-use \HWI\Bundle\OAuthBundle\OAuth\ResourceOwner\FacebookResourceOwner;
-use \Buzz\Client\Curl;
-use \HWI\Bundle\OAuthBundle\OAuth\RequestDataStorage\SessionStorage;
+use HWI\Bundle\OAuthBundle\OAuth\ResourceOwner\FacebookResourceOwner;
+use Buzz\Client\Curl;
+use HWI\Bundle\OAuthBundle\OAuth\RequestDataStorage\SessionStorage;
 
 /**
  * @DI\Service("claroline.hwi.resource_owner_factory")
@@ -40,7 +40,7 @@ class ResourceOwnerFactory
         $this->session = $session;
     }
 
-    public function getFacebook()
+    public function getFacebookResourceOwner()
     {
         $a = new Curl();
         $a->setVerifyPeer(true);

Library/Security/FacebookProvider.php
@@ -46,21 +46,7 @@ class FacebookProvider implements OAuthAwareUserProviderInterface, UserProviderI
      */
     public function loadUserByUsername($username)
     {
-        $dql = 'SELECT u FROM Claroline\CoreBundle\Entity\User u
-	    WHERE u.username LIKE :username
-	    OR u.mail LIKE :username';
-        $query = $this->em->createQuery($dql);
-        $query->setParameter('username', $username);
-
-        try {
-            $user = $query->getSingleResult();
-        } catch (NoResultException $e) {
-            throw new UsernameNotFoundException(
-                sprintf('Unable to find an active user identified by "%s".', $username)
-            );
-        }
-
-        return $user;
+        return $this->em->getRepository('ClarolineCoreBundle:User')->loadUserByUsername($username);
     }
 
     /**

Manager/HwiManager.php
@@ -44,8 +44,7 @@ class HwiManager
     public function refreshCache(RefreshCacheEvent $event)
     {
         $errors = $this->validateFacebook($this->ch->getParameter('facebook_client_id'), $this->ch->getParameter('facebook_client_secret'));
-        $boolean = count($errors) === 0 ? true: false;
-        $event->addCacheParameter('is_facebook_available', $boolean);
+        $event->addCacheParameter('is_facebook_available', count($errors) === 0);
     }
 
     public function isFacebookAvailable()

Resources/config/suggested/hwi_oauth.yml
@@ -4,8 +4,8 @@ hwi_oauth:
     resource_owners:
         facebook:
             type:          facebook
-            client_id:     "487751811261295"
-            client_secret: "571bf1838122258b9d714a9be99c7b34"
+            client_id:     <client_id>
+            client_secret: <client_secret>
             scope:         "email"
 
     # an optional setting to configure a query string parameter which can be used to redirect

commit 8c0f249f93a48d16b162014f8eafdcb0ecb72f02

Fixed config regression from 6c35dcc3e6 (closes #371)

DependencyInjection/Compiler/DynamicConfigPass.php
@@ -45,8 +45,10 @@ class DynamicConfigPass implements CompilerPassInterface
         $container->setDefinition('session.handler', $handler);
 
         //cookie lifetime
-        $storage = $container->findDefinition('session.storage');
-        $storage->addMethodCall('setOptions', array(new Reference('claroline.session.storage_options')));
+        if ($container->getParameter('kernel.environment') !== 'test') {
+            $storage = $container->findDefinition('session.storage');
+            $storage->addMethodCall('setOptions', array(new Reference('claroline.session.storage_options')));
+        }
 
         //notification
         $container->setAlias('icap.notification.orm.entity_manager', 'claroline.persistence.object_manager');